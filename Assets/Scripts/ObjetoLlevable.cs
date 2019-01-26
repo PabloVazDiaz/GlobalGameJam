0cc9e56998509db5d6dddd324cc7942c3c608e2a
Pablo Vazquez Diaz <pablo.vaz.diaz@gmail.com>
2019-01-26 17:38:17 +0100

    Commit prueba

--- Assets/Scripts/ObjetoLlevable.cs
+++ Assets/Scripts/ObjetoLlevable.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class ObjetoLlevable : MonoBehaviour
 {
+
+    public int maxTrans;
+
+    public int transformaciones;
+    public bool completado;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transformaciones = 0;
+        completado = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void Transformar()
+    {
+        Debug.Log($"{transformaciones}");
+        transformaciones++;
+        
+        if (transformaciones >= maxTrans)
+        {
+
+            completado = true;
+        }
     }
 }
